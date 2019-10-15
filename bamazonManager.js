09c009e9374c0ba6e51fcbbcc2f63974bccf2d40
Madeleine Prak <madeleineprak@gmail.com>
2019-10-14 20:32:30 -0700

    Add view low inventory function, add to inventory function and console.table package for formatting

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var keys = require("./keys");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: keys.sql.host,
@@ -11,48 +12,48 @@ var connection = mysql.createConnection({
     database: keys.sql.name
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     menuOptions();
 });
 
 function menuOptions() {
     inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View Products for Sale",
-        "View Low Inventory",
-        "Add to Inventory",
-        "Add New Product",
-        "exit"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "View Products for Sale":
-        viewProductsForSale();
-        break;
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View Products for Sale":
+                    viewProductsForSale();
+                    break;
 
-      case "View Low Inventory":
-        viewLowInventory();
-        break;
+                case "View Low Inventory":
+                    viewLowInventory();
+                    break;
 
-      case "Add to Inventory":
-        addToInventory();
-        break;
+                case "Add to Inventory":
+                    addToInventory();
+                    break;
 
-      case "Add New Product":
-        addNewProduct();
-        break;
+                case "Add New Product":
+                    addNewProduct();
+                    break;
 
-      case "exit":
-        connection.end();
-        break;
-      }
-    });
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
 
     function askNext() {
         inquirer
@@ -61,7 +62,7 @@ function menuOptions() {
                 type: "confirm",
                 message: "Would you like to do something else?"
             })
-            .then(function(answer) {
+            .then(function (answer) {
                 if (answer.confirm) {
                     menuOptions();
                 } else {
@@ -71,29 +72,59 @@ function menuOptions() {
     }
 
     function viewProductsForSale() {
-        connection.query("SELECT * FROM products", function(err, res) {
+        connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
-            console.log("SHOP");
-            console.log("============================");
-            for (var i = 0; i < res.length; i++) {
-                console.log("ID: " + res[i].item_id);
-                console.log("PRODUCT: " + res[i].product_name);
-                console.log("DEPARTMENT: " + res[i].department_name);
-                console.log("PRICE: $" + res[i].price);
-                console.log("QUANTITY: " + res[i].stock_quantity);
-                console.log("============================");
-                // console.log("ITEM ID: " + res[i].item_id + " | PRODUCT: " + res[i].product_name + " | DEPARTMENT: " + res[i].department_name + " | PRICE: $" + res[i].price + " | QUANTITY: " + res[i].stock_quantity);
-            }
+            console.table(res);
             askNext();
         });
     }
 
     function viewLowInventory() {
-
+        connection.query("SELECT * FROM products HAVING stock_quantity < 100", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            askNext();
+        })
     }
 
     function addToInventory() {
-
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            inquirer
+                .prompt([
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "What is the ID of the product you want to add to?"
+                    },
+                    {
+                        name: "units",
+                        type: "input",
+                        message: "How many units would you like to add?"
+                    }
+                ])
+                .then(function(answer) {
+                    for (var i = 0; i < res.length; i++) {
+                        if (res[i].item_id === parseInt(answer.id)) {
+                            connection.query(
+                                "UPDATE products SET ? WHERE ?", 
+                                [
+                                    {
+                                        stock_quantity: (res[i].stock_quantity += parseInt(answer.units))
+                                    },
+                                    {
+                                        item_id: parseInt(answer.id)
+                                    }
+                                ],
+                                function(err) {
+                                    if (err) throw err;
+                                }
+                            );
+                        }
+                    }
+                    askNext();
+                })
+        })
     }
 
     function addNewProduct() {
